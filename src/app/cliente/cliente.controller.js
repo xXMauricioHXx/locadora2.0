0bcfc52c61f9f937291d936c91232238a01987e1
Mauricio Henrique <mauricio.henrique@MAISTRE.LOCAL>
2019-05-13 13:11:16 -0300

    Adição de rota para listagem de todos os clientes

--- src/app/cliente/cliente.controller.js
+++ src/app/cliente/cliente.controller.js
@@ -3,11 +3,18 @@ const Cliente = require("./cliente.model");
 const insert = (req, res, next) => {
   Cliente.insert(req.body)
     .then(([cliente_id]) => {
-      res.status(200).json({cliente_id});
+      res.status(200).json({ cliente_id });
     })
     .catch(next);
 };
 
+const pesquisar = (req, res, next) => {
+  Cliente.findAll()
+    .then(clientes => res.sendStatus(200).json(clientes))
+    .catch(next);
+};
+
 module.exports = {
-  insert
-}
+  insert,
+  pesquisar
+};
--- src/app/cliente/cliente.router.js
+++ src/app/cliente/cliente.router.js
@@ -4,4 +4,5 @@ const ClienteController = require("./cliente.controller");
 const Auth = require("../../middlewares/auth");
 
 router.post("/clientes", [Auth.verify, ClienteController.insert]);
+router.get("/clientes", [Auth.verify, ClienteController.pesquisar]);
 module.exports = router;
